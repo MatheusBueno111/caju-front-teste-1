551ded454e0b2e625a63da61dc570f872706308b
Matheus Bueno <matheus.bueno.queiroz@gmail.com>
2024-09-18 20:56:41 -0300

    feat: add action to button, remove, delete, reprove, review

--- src/pages/Dashboard/components/RegistrationCard/index.tsx
+++ src/pages/Dashboard/components/RegistrationCard/index.tsx
@@ -1,37 +1,89 @@
 import { ButtonSmall } from "~/components/Buttons";
-import * as S from "./styles";
 import {
   HiOutlineMail,
   HiOutlineUser,
   HiOutlineCalendar,
   HiOutlineTrash,
 } from "react-icons/hi";
+import { usePatchStatusRegistration } from "~/hooks/usePatchStatusRegistration/usePatchStatusRegistration";
+import * as S from "./styles";
+import { Status, type Registration } from "~/types";
+import { useDeleteRegistration } from "~/hooks/useDeleteRegistration";
 
-type Props = {
-  data: any;
-};
+interface RegistrationCardProps {
+  registration: Registration;
+}
 
-const RegistrationCard = (props: Props) => {
+interface buttonMappingProps {
+  label: string;
+  status: Status;
+  color: string;
+  isVisable: boolean;
+}
+
+const RegistrationCard = ({ registration }: RegistrationCardProps) => {
+  const { patchStatusRegistrationMutate } = usePatchStatusRegistration();
+  const { deleteRegistrationMutate } = useDeleteRegistration();
+
+  const buttonMapping: buttonMappingProps[] = [
+    {
+      label: "Reprovar",
+      status: Status.REPROVED,
+      color: "rgb(255, 145, 154)",
+      isVisable: registration.status === "REVIEW",
+    },
+    {
+      label: "Aprovar",
+      status: Status.APPROVED,
+      color: "rgb(155, 229, 155)",
+      isVisable: registration.status === "REVIEW",
+    },
+    {
+      label: "Revisar novamente",
+      status: Status.REVIEW,
+      color: "#ff8858",
+      isVisable:
+        registration.status === "REPROVED" ||
+        registration.status === "APPROVED",
+    },
+  ];
+
   return (
     <S.Card>
       <S.IconAndText>
         <HiOutlineUser />
-        <h3>{props.data.employeeName}</h3>
+        <h3>{registration.employeeName}</h3>
       </S.IconAndText>
       <S.IconAndText>
         <HiOutlineMail />
-        <p>{props.data.email}</p>
+        <p>{registration.email}</p>
       </S.IconAndText>
       <S.IconAndText>
         <HiOutlineCalendar />
-        <span>{props.data.admissionDate}</span>
+        <span>{registration.admissionDate}</span>
       </S.IconAndText>
       <S.Actions>
-        <ButtonSmall bgcolor="rgb(255, 145, 154)" >Reprovar</ButtonSmall>
-        <ButtonSmall bgcolor="rgb(155, 229, 155)">Aprovar</ButtonSmall>
-        <ButtonSmall bgcolor="#ff8858">Revisar novamente</ButtonSmall>
-
-        <HiOutlineTrash />
+        {buttonMapping.map(({ color, label, status, isVisable }) =>
+          isVisable ? (
+            <ButtonSmall
+              key={status}
+              bgcolor={color}
+              onClick={() => {
+                patchStatusRegistrationMutate({
+                  id: registration.id,
+                  status: status,
+                });
+              }}
+            >
+              {label}
+            </ButtonSmall>
+          ) : null
+        )}
+        <HiOutlineTrash
+          onClick={() => {
+            deleteRegistrationMutate({ id: registration.id });
+          }}
+        />
       </S.Actions>
     </S.Card>
   );
