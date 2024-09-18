4478c23cd148e8a3c9142889960cacb944ffb3c7
Matheus Bueno <matheus.bueno.queiroz@gmail.com>
2024-09-18 20:57:14 -0300

    feat: add fetchers

--- src/services/registrationFetchers/index.ts
+++ src/services/registrationFetchers/index.ts
@@ -1,8 +1,18 @@
-import { cpfRemoveMask } from "~/utils/cpfRemoveMask";
 import { api } from "../api";
+import type { Status } from "~/types";
 
-export const getRegistrations = async (search = "") => {
-  const formattedSearch = cpfRemoveMask(search);
-  const { data } = await api.get(`/registrations?cpf=${formattedSearch}`);
+export const getRegistrations = async (search: string = "") => {
+  const { data } = await api.get(`/registrations?cpf=${search}`);
   return data;
+};
+
+export const patchStatusRegistration = async (id: string, status: Status) => {
+  const { data } = await api.patch(`/registrations/${id}`, { status });
+  return data;
+};
+
+export const deleteRegistration = async (id: string) => {
+  const response = await api.delete(`/registrations/${id}`);
+  console.log(response);
+  return response.data;
 };
